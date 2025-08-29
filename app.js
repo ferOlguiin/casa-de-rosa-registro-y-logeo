b832945c8bf8a5345c558e79a533628e729461bc
Ch3f-Ry4n <feergui997@hotmail.com>
2025-08-29 00:01:43 -0300

    nuevoscambios000129082025

--- app.js
+++ app.js
@@ -9,8 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-// app.use(cors({origin: ["https://www.casaderosaonline.com.ar", "https://casaderosaonline.com.ar"]}));
-app.use(cors({origin: ["http://localhost:5173",], credentials: true}));
+app.use(cors({origin: ["https://www.casaderosaonline.com.ar", "https://casaderosaonline.com.ar", "https://*.casaderosaonline.com.ar",], credentials: true}));
   
 app.use(router);
 
